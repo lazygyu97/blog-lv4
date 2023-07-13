00b2ef1229089c7dd9b3120ad2906a85e636b9ba
Park Hyeongyu <park970406@me.com>
2023-07-14 00:29:58 +0900

    init commit- 레벨 2,3 구현 및 레벨 4 과제 수행

---
+++ src/main/java/com/example/bloglv4/BlogLv4Application.java
@@ -0,0 +1,13 @@
+package com.example.bloglv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogLv4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogLv4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bloglv4/BlogLv4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloglv4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogLv4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
