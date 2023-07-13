5465c8d69c44646e7327242c344657af74eaa31c
Park Hyeongyu <park970406@me.com>
2023-07-14 07:24:15 +0900

    Level.4 : 댓글 좋아요 관련 기능 구현 및 글 조회시 작성일 기준 내림차순 조회 적용 + 주석 추가 커밋

--- src/main/java/com/example/bloglv4/BlogLv4Application.java
+++ src/main/java/com/example/bloglv4/BlogLv4Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+@EnableJpaAuditing // 타임스탬프를 사용하기 위한 어노테이션
 @SpringBootApplication
 public class BlogLv4Application {
 
